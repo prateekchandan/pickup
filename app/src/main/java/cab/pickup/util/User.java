aefa6a63b619d106935c0d883c8292f224df3fd1
Meet Udeshi <mudeshi1209@gmail.com>
2015-03-17 18:55:49 +0530

    Fix  generated json  syntax

--- app/src/main/java/cab/pickup/util/User.java
+++ app/src/main/java/cab/pickup/util/User.java
@@ -1,10 +1,13 @@
 package cab.pickup.util;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User{
     public String id, fbid, device_id, name, email, gender,company,mobile,age,company_email;
+    public LatLng position;
 
 
     public User(JSONObject user) throws JSONException{
@@ -33,13 +36,18 @@ public class User{
         json+="\"device_id\":\""+device_id+"\",";
         json+="\"first_name\":\""+name+"\",";
         json+="\"email\":\""+email+"\",";
-        json+="\"gender\":\""+gender+"\"";
-        json+="\"comapany\":\""+company+"\"";
-        json+="\"mobile\":\""+mobile+"\"";
-        json+="\"age\":\""+age+"\"";
+        json+="\"gender\":\""+gender+"\",";
+        json+="\"comapany\":\""+company+"\",";
+        json+="\"mobile\":\""+mobile+"\",";
+        json+="\"age\":\""+age+"\",";
         json+="\"company_email\":\""+company_email+"\"";
         json+="}";
 
         return json;
+    }
+
+    public void setPosition(String position) {
+        int comma = position.indexOf(',');
+        this.position = new LatLng(Double.valueOf(position.substring(0,comma)), Double.valueOf(position.substring(comma+1)));
     }
 }
