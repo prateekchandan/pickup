dcb19fa9d634f3313210eb130162456de4d4268b
Prateek Chandan <prateekchandan5545@gmail.com>
2015-07-26 01:17:29 +0530

    setting up of profile page

--- app/src/main/java/cab/pickup/ui/activity/ProfileActivity.java
+++ app/src/main/java/cab/pickup/ui/activity/ProfileActivity.java
@@ -1,23 +1,45 @@
 package cab.pickup.ui.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
 
 import cab.pickup.R;
 
 public class ProfileActivity extends MyActivity{
 
+    ImageView mProfilePic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#22000000")));
+        try {
+            getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.profile_actionbar_gradient));
+        }
+        catch (Exception E){
+            E.printStackTrace();
+        }
         setContentView(R.layout.activity_profile);
+        mProfilePic = (ImageView)findViewById(R.id.profile_picture);
+        setProfilePicture();
+        setProfileInfo();
     }
 
     @Override
@@ -25,6 +47,12 @@ public class ProfileActivity extends MyActivity{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_profile, menu);
         return true;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        setProfileInfo();
     }
 
     @Override
@@ -40,5 +68,62 @@ public class ProfileActivity extends MyActivity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setProfilePicture(){
+        new AsyncTask<String,Void,Bitmap>(){
+
+            @Override
+            protected Bitmap doInBackground(String... strings) {
+                File img =  new File(getFilesDir()+"/profile.jpg");
+                Bitmap bmp= null;
+                if(img.exists()){
+                    try {
+                        bmp = BitmapFactory.decodeFile(img.getAbsolutePath());
+                    }
+                    catch (Exception E){
+                        E.printStackTrace();
+                    }
+                }else{
+                    try {
+                        URL img_url = new URL("https://graph.facebook.com/" + String.valueOf(me.fbid) + "/picture?type=large&redirect=true&width=400&height=400");
+                        bmp = BitmapFactory.decodeStream(img_url.openConnection().getInputStream());
+                        FileOutputStream fOut = new FileOutputStream(img);
+                        bmp.compress(Bitmap.CompressFormat.JPEG, 90, fOut);
+                        fOut.flush();
+                        fOut.close();
+                    }
+                    catch (Exception E){
+                        E.printStackTrace();
+                    }
+                }
+                return bmp;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap img){
+                if(img!=null){
+                    mProfilePic.setImageBitmap(img);
+                }
+            }
+        }.execute();
+    }
+
+    public void setProfileInfo(){
+        ((TextView)findViewById(R.id.profile_name)).setText(me.name);
+        ((TextView)findViewById(R.id.profile_name_1)).setText(me.name);
+        ((TextView)findViewById(R.id.profile_email)).setText(me.email);
+        ((TextView)findViewById(R.id.profile_phone)).setText(me.mobile);
+        ((TextView)findViewById(R.id.profile_gender)).setText(me.gender);
+        String age_gender = String.valueOf(me.age);
+        if(me.gender.equals("male"))
+            age_gender+=" M";
+        else if(me.gender.equals("female"))
+            age_gender+=" F";
+        ((TextView)findViewById(R.id.profile_age_gender)).setText(age_gender);
+    }
+
+    public void editProfile(View v){
+        startActivity(new Intent(this,SettingsActivity.class));
     }
 }
