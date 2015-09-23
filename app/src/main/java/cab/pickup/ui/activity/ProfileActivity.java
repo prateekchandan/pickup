84089e19df0073ea5195bda5a949ed538eb7fa71
Prateek Chandan <prateekchandan5545@gmail.com>
2015-09-24 01:43:00 +0530

    Fix in profile activity -dded id to toolbar and fixed crash

--- app/src/main/java/cab/pickup/ui/activity/ProfileActivity.java
+++ app/src/main/java/cab/pickup/ui/activity/ProfileActivity.java
@@ -34,14 +34,15 @@ public class ProfileActivity extends MyActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_profile);
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        setContentView(R.layout.activity_profile);
+
         mProfilePic = (ImageView)findViewById(R.id.profile_picture);
         setProfilePicture();
         setProfileInfo();
